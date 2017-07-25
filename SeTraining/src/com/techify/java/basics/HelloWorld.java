86f353071b0ee061ee49f550cba5a791068e9b75
admin <admin@admin-PC>
2017-07-25 09:35:47 +0530

    Changes

---
+++ SeTraining/src/com/techify/java/basics/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.techify.java.basics;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		int age = 27;
+		System.out.println(age);
+		
+		age = age+3;
+		System.out.println(age);
+
+	}
+
+}
