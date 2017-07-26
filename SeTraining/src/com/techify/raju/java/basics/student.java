4bc9e40eeeaf7bf4f79425eb9a78cda4e24de550
rajuramadugu <rajuramadugu@rajunani>
2017-07-26 23:32:34 +0530

    created object for student

---
+++ SeTraining/src/com/techify/raju/java/basics/student.java
@@ -0,0 +1,21 @@
+package com.techify.raju.java.basics;
+
+public class student {
+	String name;
+	double id;
+	String schoolName;
+	public void display(String name,double id,String schoolName){
+		
+	}
+
+	public static void main(String[] args) {
+		student stu=new student();
+		stu.display("praveen",1220,"oxford");
+		
+		
+		
+
+
+	}
+
+}
