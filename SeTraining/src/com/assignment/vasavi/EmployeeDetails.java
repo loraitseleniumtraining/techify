bf92b3a0cc60c6810d080400a6e696c873deebfe
vasavi <vasavi@192.168.1.17>
2017-07-27 12:16:07 +0530

    Organization aasignment

---
+++ SeTraining/src/com/assignment/vasavi/EmployeeDetails.java
@@ -0,0 +1,51 @@
+package com.assignment.vasavi;
+
+
+public class EmployeeDetails {
+	
+	
+		String empName;
+		String empRole;
+		String movie;
+
+		public static void main(String[] args) {
+			Bahubali();
+			System.out.println("--------------------");
+			
+			EmployeeDetails dir = new EmployeeDetails();
+			dir.empName = "rajmouli";
+			dir.empRole = "director";
+			dir.film();
+			EmployeeDetails hero = new EmployeeDetails();
+			hero.empName = "prabhas";
+			hero.empRole = "hero";
+			hero.film();
+			EmployeeDetails villan = new EmployeeDetails();
+			villan.empName = "Rana daggubati";
+			villan.empRole = "villan";
+			villan.film();
+			EmployeeDetails her = new EmployeeDetails();
+			her.empName = "Anushka";
+			her.empRole = "heroine";
+			her.film();
+			
+			
+		}
+		public void film(){
+			 
+			System.out.println(empName+" | "+empRole);
+			System.out.println("--------------------");
+		//	System.out.println()
+			}
+		public static void Bahubali(){
+			 
+			System.out.println("Film Crew");
+			
+			}
+		
+	
+
+
+}
+
+	
