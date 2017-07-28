d79d05b4115ca81b10ca820a380d8ca3e24cbdf8
Rohit Sharma <Rohit Sharma@LAPTOP-COOS8D4J>
2017-07-28 21:18:51 +0530

    Hello

--- SeTraining/src/com/techify/rohit/java/basis/Hello.java
+++ SeTraining/src/com/techify/rohit/java/basis/Hello.java
@@ -2,4 +2,9 @@ package com.techify.rohit.java.basis;
 
 public class Hello {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+	}
+
 }
