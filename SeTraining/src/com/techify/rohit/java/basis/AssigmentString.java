a22fb7ef5f9884f4484becfa9f1191ba12dba52b
Rohit Sharma <Rohit Sharma@LAPTOP-COOS8D4J>
2017-08-03 09:26:10 +0530

    Added my scripts

---
+++ SeTraining/src/com/techify/rohit/java/basis/AssigmentString.java
@@ -0,0 +1,10 @@
+package com.techify.rohit.java.basis;
+
+public class AssigmentString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SeTraining/src/com/techify/rohit/java/basis/Fibonacci.java
@@ -0,0 +1,17 @@
+package com.techify.rohit.java.basis;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int a = 0, b = 1, c;
+		System.out.println(a);
+		c = a + b;
+		System.out.println(b);
+		while (c < 20) {
+			a = b;
+			b = c;
+			c = a + b;
+			System.out.println(c);
+		}
+	}
+}
