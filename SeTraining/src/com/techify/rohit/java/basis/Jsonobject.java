d104427d54eba77d8e16401cd9123ef1d4b9274c
Rohit Sharma <Rohit Sharma@LAPTOP-COOS8D4J>
2017-08-10 19:03:50 +0530

    Json not working program

---
+++ SeTraining/src/com/techify/rohit/java/basis/Jsonobject.java
@@ -0,0 +1,13 @@
+package com.techify.rohit.java.basis;
+
+public class Jsonobject {
+
+	public void put(String string, String age) {
+			String name = "Ravi", age="25";//local variable
+			String name2 = "Prakash", age2="26";
+			Jsonobject obj = new Jsonobject();
+			obj.put("name", name);
+			obj.put("name", age);
+		}		
+	}
+
