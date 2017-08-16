88adb994c6101ac640fa6c130eaf2bffc1dd9ebe
rajuramadugu <rajuramadugu@rajunani>
2017-08-17 00:29:21 +0530

    created commerce xpath

---
+++ SeTraining/src/com/techify/selenium/raju/Commerce.java
@@ -0,0 +1,18 @@
+package com.techify.selenium.raju;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Commerce {
+	public void openApplication(){
+		System.setProperty("Webdriver.gecko.driver",System.getProperty("user.dir") + "\\SeTraining\\src\\lib\\geckodriver.exe");
+		WebDriver driver= new FirefoxDriver();
+		driver.get("http://demo.nopcommerce.com/register");
+		driver.findElement(By.xpath("//a[@class='ico-register']"));
+	
+		
+		
+	}
+
+}
